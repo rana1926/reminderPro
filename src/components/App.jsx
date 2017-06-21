e37a07db574d2272e5872c9c5b138ff7dd226aa7
Rana Kelani <rana.1926@hotmail.com>
2017-06-21 12:13:50 +0300

    Add simple form

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,7 +3,21 @@ import React, { Component } from 'react';
 class App extends Component{
 	render(){
 		return (
-			<div>Reminder App</div>
+			<div className="App">
+				<div className="title">
+					Reminder Pro
+				</div>
+				<div className="form-inline">
+					<div className="form-group">
+						<input
+							className="form-control"
+							type="text"
+							placeholder="I have to..."
+						/>
+					</div>
+					<button className="btn btn-success" type="button">Add Reminder</button>
+				</div>
+			</div>
 		)
 	}
 }
