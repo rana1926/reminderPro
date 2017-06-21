42cb33d508c790dc56e521351890dc60aa7e83b4
Rana Kelani <rana.1926@hotmail.com>
2017-06-21 14:25:05 +0300

    Dispatch actionCreators to props

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,17 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { addReminder} from '../actions';
 
 class App extends Component{
+	constructor(props) {
+		super(props);
+		this.state = {
+			text: ''
+		}
+	}
+	addReminder(){
+		this.props.addReminder(this.state.text);
+	}
 	render(){
 		return (
 			<div className="App">
@@ -13,13 +24,14 @@ class App extends Component{
 							className="form-control"
 							type="text"
 							placeholder="I have to..."
+							onChange={event => this.setState({text: event.target.value})}
 						/>
 					</div>
-					<button className="btn btn-success" type="button">Add Reminder</button>
+					<button className="btn btn-success" type="button" onClick={()=>this.addReminder()}>Add Reminder</button>
 				</div>
 			</div>
 		)
 	}
 }
 
-export default App;
+export default connect(null, {addReminder})(App);
